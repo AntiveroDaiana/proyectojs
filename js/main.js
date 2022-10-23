c18aca29d5305cb6a186a386034f52944c6d94eb
daianajantivero <daiana.antivero1994@gmail.com>
2022-10-23 00:01:05 -0300

    correccion de archivo js

--- js/main.js
+++ js/main.js
@@ -1,18 +1,22 @@
+
  const edad = 18;
 
  let entrada = alert ("Bienvenidx a Linkedog, aqui respetamos a nuestros amigos y somos responsables a la hora de realizar la solicitud de adopcion.Seamos empaticos y concientes!");
- let ingreso = prompt ("Ingrese su edad");
+ let ingreso = parseInt( prompt ("Ingrese su edad."));
  let coincide = (edad <= ingreso);
-
- if (coincide) {
-    alert ("Ahora si, a adoptar!");
- }else{ 
-    alert ("Eres menor, debes hablar con tus mayores.");
-    window.location.src = "../pages/menor.html";
- }
+ let repetir = true;
 
 
-for (let i=0; i <=16; i=i+2){
-    console.log(i);
-}
-
+while (repetir) {
+   if (coincide) {
+      alert ("Ahora si, a adoptar!");
+      repetir= false;
+  
+     }else if (ingreso<edad){ 
+      alert ("Eres menor, debes hablar con tus mayores.");
+      repetir= false;
+     }else {
+      ingreso = parseInt( prompt ("Por favor, ingrese un numero valido"));
+     }
+}   
+   

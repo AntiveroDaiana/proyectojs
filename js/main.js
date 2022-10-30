fa965490588a2629763218579e591eb1c4736180
daianajantivero <daiana.antivero1994@gmail.com>
2022-10-30 18:56:55 -0300

    Segunda pre entrega
    Agregue : Una array con objetos, en este caso con mascotas para adoptar con sus caracteristicas, y utilice el metodo de busqueda FIND()

--- js/main.js
+++ js/main.js
@@ -25,3 +25,77 @@ while (repetir){
       repetir = false;
    }
 }
+
+// Array con objeto //
+const perrosEnAdopcion = [
+   {
+       id: 1,
+       nombre: "Mara",
+       edad: 9,
+       Tamaño: 'mediana',
+       Salud: "Castrada y vacunada",
+       Social: "Se lleva bien con otros animales"
+   },
+   {
+      id: 2,
+      nombre: "Pamela",
+      edad: 2,
+      Tamaño: 'mediana',
+      Salud: "Castrada y vacunada",
+      Social: "Se lleva bien con otros animales"
+   },
+   {
+      id: 3,
+      nombre: "Paulita",
+      edad: "4 meses",
+      Tamaño: 'Indefinido',
+      Salud: "Castrada y vacunada",
+      Social: "Se lleva bien con otros animales"
+   },
+   {
+      id: 4,
+      nombre: "Voldemort",
+      edad: 3/4,
+      Tamaño: 'Grande',
+      Salud: "Castrado y vacunado",
+      Social: "Se lleva bien con otros animales"
+   },
+   {
+      id: 5,
+      nombre: "Yumina",
+      edad: "6 meses",
+      Tamaño: 'mediana',
+      Salud: "Castrada y vacunada",
+      Social: "Se lleva bien con otros animales"
+   },
+   {
+      id: 6,
+      nombre: "Ágata",
+      edad: 4,
+      Tamaño: 'mediana',
+      Salud: "Castrada y vacunada",
+      Social: "Se lleva bien con otros animales"
+   },
+   {
+      id: 7,
+      nombre: "Dominga",
+      edad: 7/8,
+      Tamaño: 'Grande',
+      Salud: "Castrada y vacunada",
+      Social: "Se lleva bien con otros animales"
+   },
+   {
+      id: 8,
+      nombre: "Chuleta",
+      edad: 6,
+      Tamaño: 'Pequeña',
+      Salud: "Castrada y vacunada",
+      Social: "Se lleva bien con otros animales"
+   },
+];
+
+// metodo de busqueda con FIND 
+let valorBuscado = "Mara"
+const busqueda = perrosEnAdopcion.find(perrosEnAdopcion => perrosEnAdopcion.nombre === valorBuscado)
+console.log(busqueda)
+

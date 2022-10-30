1088625aec377d8b998e4b8ac76d62f3ada7e763
daianajantivero <daiana.antivero1994@gmail.com>
2022-10-30 17:55:08 -0300

    Function agregada
    Se agrego una function y se mejoro el codigo

--- js/main.js
+++ js/main.js
@@ -1,23 +1,27 @@
 
  const edad = 18;
 
- let entrada = alert ("Bienvenidx a Linkedog, aqui respetamos a nuestros amigos y somos responsables a la hora de realizar la solicitud de adopcion.Seamos empaticos y concientes!");
+ const entrada = alert ("Bienvenidx a Linkedog, aqui respetamos a nuestros amigos y somos responsables a la hora de realizar la solicitud de adopcion.Seamos empaticos y concientes!💘​💫​");
  let ingreso = parseInt( prompt ("Ingrese su edad."));
- let coincide = (edad <= ingreso);
  let repetir = true;
 
+function coincide (ingreso) {
+   return (18 <= ingreso)
+};
 
-while (repetir) {
-   if (coincide) {
-      alert ("Ahora si, a adoptar!");
-      repetir= false;
-  
-     }else if (ingreso<edad){ 
-      alert ("Eres menor, debes hablar con tus mayores.");
-      repetir= false;
-     }else {
-      ingreso = parseInt( prompt ("Por favor, ingrese un numero valido"));
-      coincide = (edad <= ingreso);
-     }
-}   
-   
+if (coincide(ingreso)) {
+      alert ("Listo, a adoptar! 💖​🐶​🐾​");
+      repetir = false;
+}
+while (repetir){
+   if(isNaN(ingreso)){
+      ingreso = parseInt(prompt("Por favor, ingrese un número válido!❌​"));
+      continue
+   }else if(coincide(ingreso)) {
+      alert ("Listo, a adoptar! 💖​🐶​🐾​");
+      repetir = false;
+   }else{ 
+      alert ("⚠️​Eres menor, debes hablar con tus mayores.⚠️​");
+      repetir = false;
+   }
+}
